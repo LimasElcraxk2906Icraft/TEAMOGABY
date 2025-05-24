953b86e4496b37b92df07052242450ec1dbcfb5f
LimasElcraxk2906Icraft <141187237+LimasElcraxk2906Icraft@users.noreply.github.com>
2025-05-24 13:29:19 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import { login, signup, logout } from './auth.js'
-import { startGame } from './game.js'
-
-window.login = login
-window.signup = signup
-window.logout = logout
-window.startGame = startGame
